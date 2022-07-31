1aa315f4c6315705db9c7e2a0f8be3f696e69f78
mahditoosi <pugnacious0571@gmail.com>
2022-07-31 06:29:16 +0430

    feat: implement axios instance

---
+++ src/utils/api.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { history as router } from '@/router'
+import toast from '@/utils/toast'
+const baseURL = import.meta.env?.VITE_BASE_URL
+
+const axiosInstance = axios.create({ baseURL })
+
+function handleRequest(config: AxiosRequestConfig) {
+	const token = localStorage.getItem('token')
+	if (token) {
+		config.headers!['Authorization'] = token
+	}
+
+	return config
+}
+
+function handleResponse(response: AxiosResponse) {
+	return response.data
+}
+
+axiosInstance.interceptors.request.use(handleRequest)
+
+axiosInstance.interceptors.response.use(handleResponse, (error) => {
+	if (!error) return
+
+	if (error.response?.status === 401 && window.location.pathname !== '/login') {
+		localStorage.clear()
+		router.push('/login')
+		return
+	}
+
+	toast(error.response?.data.message || error.message)
+})
+
+export default axiosInstance
